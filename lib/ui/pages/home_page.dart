4c27b0882c85b7c07d74b33a9205c77dcf9b1e79
Juan David Maestre Ramirez <maestrejd@uninorte.edu.co>
2025-03-26 19:42:03 -0500

    feat: add event_Card widget & install GetX

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import '../widgets/event_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Eventos Próximos")),
-      body: Center(child: Text("Lista de eventos aquí")),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text("Hi Coder! There are some webinars incoming"),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  EventCard(
+                      eventName: "React Course", eventCategorie: "Developer"),
+                  EventCard(
+                      eventName: "Java Course", eventCategorie: "Developer"),
+                  EventCard(
+                      eventName: "Css Course", eventCategorie: "Developer"),
+                  EventCard(
+                      eventName: "HTML Course", eventCategorie: "Developer")
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
