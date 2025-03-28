67cff00e5b7a9846321cd35d60a163c5a7154c63
JuandiGo1 <98894997+JuandiGo1@users.noreply.github.com>
2025-03-27 22:43:17 -0500

    fix: update date formatter

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -1,5 +1,6 @@
 import 'package:intl/intl.dart';
 
 String formatDate(DateTime dateTime) {
-  return DateFormat('E, d MMM', 'es_ES').format(dateTime);
+  String formattedDate = DateFormat('E, d MMM', 'es_ES').format(dateTime);
+  return toBeginningOfSentenceCase(formattedDate)!;
 }
