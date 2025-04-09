e87bf44e4ca0dd77419a2d80876526cf9a385c2d
Almor21 <98895136+Almor21@users.noreply.github.com>
2025-04-08 21:45:54 -0500

    feat: add route to event detail

--- lib/ui/widgets/enventLines/card_event.dart
+++ lib/ui/widgets/enventLines/card_event.dart
@@ -1,18 +1,15 @@
 import 'package:confhub/core/colors.dart';
+import 'package:confhub/ui/controllers/event_page_controller.dart';
+import 'package:confhub/ui/pages/event_detail_page.dart';
 import 'package:flutter/material.dart';
+import 'package:confhub/domain/entities/event.dart';
+import 'package:get/get.dart';
 
 class CardEvent extends StatelessWidget {
-  final int eventAmount;
-  final String title;
-  final String date;
+  final Event event;
   final int color;
 
-  const CardEvent(
-      {super.key,
-      required this.eventAmount,
-      required this.title,
-      required this.color,
-      required this.date});
+  const CardEvent({super.key, required this.color, required this.event});
 
   Color _getColorFromCode(int code) {
     switch (code) {
@@ -31,59 +28,83 @@ class CardEvent extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color mColor = _getColorFromCode(color);
 
-    return Container(
-      width: 140,
-      height: 140,
-      decoration: BoxDecoration(
-        color: mColor,
-        border: Border.all(
-          color: Colors.black,
-          width: 1.0,
-        ),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        children: [
-          Expanded(
-              child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Text(title,
-                      style: TextStyle(
-                          color: AppColors.textPrimary,
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold)))),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  color: AppColors.background, // Color del borde
-                  width: 1.0, // Grosor del borde
-                ),
-              ),
-            ),
-            child: Text(
-              date,
-              style: TextStyle(
-                color: AppColors.background,
-                backgroundColor: Colors.transparent,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Get.to(
+          () => EventDetailPage(
+            eventTitle: event.title,
+            eventId: event.eventid,
+            eventDate: event.date,
+            eventAttendees: event.attendees,
+            eventCategory: event.category,
+            eventDescription: event.description,
+            eventSpeakerAvatar: event.speakerAvatar,
+            eventTime: event.time,
+            eventSpeakerName: event.speakerName,
+            eventLocation: event.location,
+            eventSpots: event.availableSpots,
+            colorName: mColor,
           ),
-          Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 30,
+          transition: Transition.cupertino,
+        );
+        
+        Get.put<EventPageController>(EventPageController(
+            initialAttendees: event.attendees, initialSpots: event.availableSpots));
+      },
+      child: Container(
+        width: 140,
+        height: 140,
+        decoration: BoxDecoration(
+          color: mColor,
+          border: Border.all(
+            color: Colors.black,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(event.title,
+                        style: TextStyle(
+                            color: AppColors.textPrimary,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold)))),
+            Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(12),
-                  bottomRight: Radius.circular(12),
+                border: Border(
+                  top: BorderSide(
+                    color: AppColors.background, // Color del borde
+                    width: 1.0, // Grosor del borde
+                  ),
                 ),
               ),
               child: Text(
-                '$eventAmount en vivo',
-                style: TextStyle(color: AppColors.textPrimary),
-              ))
-        ],
+                event.date,
+                style: TextStyle(
+                  color: AppColors.background,
+                  backgroundColor: Colors.transparent,
+                ),
+              ),
+            ),
+            Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  '${event.attendees} en vivo',
+                  style: TextStyle(color: AppColors.textPrimary),
+                ))
+          ],
+        ),
       ),
     );
   }
